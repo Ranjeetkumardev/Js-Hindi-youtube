9429814932cf692bd2e2c6928ea38cd6ac2ce23a
dev razz <masterrazz363@gmail.com>
2023-09-30 18:36:23 -0400

    Datetypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+"use strict mode"; // treaat all code as newer version of js
+
+//alert(3 + 5) // don't work ,becouse we use Node js not browser;
+
+const age = 85
+ str = "Hello ranjeet"
+
+
+//DataTypes
+// Number => 2 to power 54
+//bigint => use for big value
+//boolean => true/false
+//null=> standalone value type object()
+//undefined => not define => type undefined
+//symbol => unique
+
+//object 
+//console.log(typeof str ) // fun to check type of datatypes 
