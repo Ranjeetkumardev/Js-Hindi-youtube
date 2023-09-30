7ff1899d8c5ddd36994ba7fd83db1032efabf410
dev razz <masterrazz363@gmail.com>
2023-09-30 19:35:19 -0400

    modified Datatypes conversion

--- 03_Datatype_conversion.js
+++ 03_Datatype_conversion.js
@@ -23,5 +23,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********************************operations****************************************
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // 2 to power 3
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello";
+let str2 = " Ranjeet"; // ye space count hota h
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1"+2) //12
+// console.log(1+ "2") //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(true) // trrue
+// console.log(+true) // 1
+// console.log("")  // 0
