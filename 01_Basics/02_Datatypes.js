db3e1c8ba65fa65d09e7010b8bbdba9e344081fc
dev razz <masterrazz363@gmail.com>
2023-09-30 21:35:01 -0400

    modifide Datatypes

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -14,5 +14,41 @@ const age = 85
 //undefined => not define => type undefined
 //symbol => unique
 
-//object 
-//console.log(typeof str ) // fun to check type of datatypes 
+//object
+//console.log(typeof str ) // fun to check type of datatypes
+
+
+
+
+
+//*********************datatypes summary********** */
+// Primitive Datatypes
+
+// 7 types : String ,Number ,Boolean ,null ,underdefined ,Symbol ,Bigint 
+const score = 100
+const scoreValue = 100.4
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123') //Look like same but not same thats beauty of Sybole they are uniqiue
+
+const binNumber = 6532589725225n;
+
+
+// Reference (Non primitive)
+
+//Array , objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "hitesh",
+    age : 22
+}
+
+const myFunction = function () {
+    console.log("Hello world")
+}
+
+console.log(typeof binNumber) // undifined
